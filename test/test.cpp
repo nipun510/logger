410871eb0c6bb4ac2cae1a3a2a660c75df6f2ba4
nipun <nipunkumar66@gmail.com>
2018-11-24 19:22:07 +0530

    code to test thread safety logger

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,31 @@
 
 #include <logger.hpp>
 #include <iostream>
+#include <fstream>
+#include <thread>
 
 int main()
 {
-    logger::logger l;
-    l.addstream(std::cout, true);
-    l.info("Hello Word");
-    l.error("Error detectred");
-    l.warn("Memory exhausted");
+    
+    info("Hello Word");
+    
+    error("Error detectred");
+
+    std::thread t1([](){info("Running from inside the thread");});
+    std::thread t2([](){info("Running from inside the thread");});
+    std::thread t3([](){info("Running from inside the thread");});
+    warn("Memory exhausted");
+    std::thread t4([](){info("Running from inside the thread");});
+    std::thread t5([](){info("Running from inside the thread");});
+    std::thread t6([](){info("Running from inside the thread");});
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+
+
 
 }
